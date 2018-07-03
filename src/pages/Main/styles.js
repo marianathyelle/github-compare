3869574d211c91c041f933f03b13569fac98253e
Maria Nathyelle <marianathyelle@nalaje.com>
2018-07-03 06:24:20 -0300

    app github-compare

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 60px;
+`;
+
+export const Form = styled.form`
+  margin-top: 20px;
+  width: 100%;
+  max-width: 400px;
+  display: flex;
+
+  input,
+  button {
+    height: 55px;
+    padding: 0 20px;
+    border: 0;
+    border-radius: 3px;
+  }
+
+  input {
+    flex: 1;
+    background: #fff;
+    font-size: 18px;
+    color: #444;
+  }
+
+  button {
+    background-color: #63f5b6;
+    color: #fff;
+    margin-left: 10px;
+    font-size: 20px;
+    font-weight: 700;
+
+    &:hover {
+      background-color: #52d89f;
+    }
+  }
+`;
---
+++ src/styles/global.js
@@ -0,0 +1,17 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    background: #7957FF;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    font-family: sans-serif;
+  }
+`;
